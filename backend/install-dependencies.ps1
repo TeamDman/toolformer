ed3b359d0d1c548be8b8d8671302027cff4a1c67
TeamDman <TeamDman9201@gmail.com>
2023-03-10 00:02:18 -0500

    add some files

--- backend/install-dependencies.ps1
+++ backend/install-dependencies.ps1
@@ -2,3 +2,10 @@ python -m pip install --upgrade pip
 pip install ipykernel
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu117
 pip install websockets SpeechRecognition openai-whisper pipwin pyaudio accelerate transformers sympy fuzzywuzzy screeninfo pip install lovely-tensors
+
+# pip install -r https://github.com/coqui-ai/TTS/raw/dev/requirements.txt
+# pip install TTS
+pip install git+https://github.com/coqui-ai/TTS
+pip install pyinstaller
+pip install numba==0.56.4
+pip install numpy==1.23.5
