b383b20eba3bd3ce3cd95100bc92e85f5b7eba93
TeamDman <TeamDman9201@gmail.com>
2023-03-12 22:21:46 -0400

    better retry logic on repl

--- backend/src/main_text_repl.py
+++ backend/src/main_text_repl.py
@@ -5,17 +5,17 @@ import websockets.client
 import lifecycle
 
 async def main():
-    try:
-        stop_future = lifecycle.create_lifecycle()
-        async with websockets.client.connect(f"ws://localhost:{constants.text_websocket_port}") as ws:
-            while not stop_future.done():
-                prompt = input("Prompt: ")
-                await ws.send(json.dumps({"prompt": prompt, "stop_token": "\n"}))
+    stop_future = lifecycle.create_lifecycle()
+    while not stop_future.done():
+        try:
+            prompt = input("Prompt: ")
+            async with websockets.client.connect(f"ws://localhost:{constants.text_websocket_port}") as ws:
+                await ws.send(prompt)
                 response = await ws.recv()
                 print(f"Response: {response}")
-    except websockets.client.ConnectionClosedError:
-        print("Connection closed")
-        exit(0)
+        except Exception as e:
+            print(f"Error connecting to websocket: {e}")
+            await asyncio.sleep(1)
 
 if __name__ == "__main__":
     asyncio.run(main())
